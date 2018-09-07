f07a6ad3d9d6cb33612a39dcbd7e09598199d478
Roldán García <jroldang@indra.es>
2018-09-07 10:10:51 +0200

    testing function component

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -2,14 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Hello(props){
+  return <p>{props.titulo}</p>
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Hello titulo='function component 1'/>
         </header>
+        <Hello titulo='function component 2'/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
